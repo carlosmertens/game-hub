ca5138beb2745e645a60f5e4b36a846cd02ef15d
Carlos Mertens <mertenscarlos3000@gmail.com>
2025-10-31 18:53:25 +0100

    Add Chakra color mode functionality

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,16 @@
-import { HStack, Image, Text } from '@chakra-ui/react';
+import { Button, HStack, Image, Text } from '@chakra-ui/react';
 import logo from '../assets/logo.webp';
+import { useColorMode } from '@/components/ui/color-mode';
 
 const NavBar = () => {
+  const { toggleColorMode } = useColorMode();
   return (
     <HStack>
       <Image src={logo} boxSize='60px' />
       <Text>Navbar</Text>
+      <Button variant='outline' onClick={toggleColorMode}>
+        Toggle Mode
+      </Button>
     </HStack>
   );
 };
