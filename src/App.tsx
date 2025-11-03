c7e9c4a82e9f7bce439c24cc1e4044c904ec2eaa
Carlos Mertens <mertenscarlos3000@gmail.com>
2025-11-03 15:04:18 +0100

    Build games grid with data fetched

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Grid, GridItem } from '@chakra-ui/react';
 import NavBar from './components/NavBar';
+import GameGrid from './components/GameGrid';
 
 function App() {
   return (
@@ -15,8 +16,10 @@ function App() {
       <GridItem area='aside' bg='gold' hideBelow='lg'>
         aside
       </GridItem>
-      <GridItem area='main' bg='dodgerblue'>
-        Main
+
+      <GridItem area='main'>
+        <h2>Main Area Grid</h2>
+        <GameGrid />
       </GridItem>
     </Grid>
   );
