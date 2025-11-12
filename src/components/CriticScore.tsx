000d44ab183e4bea6432b830fda9f8a78e6ab491
Carlos Mertens <mertenscarlos3000@gmail.com>
2025-11-12 14:04:04 +0100

    Add critic score badge

---
+++ src/components/CriticScore.tsx
@@ -0,0 +1,17 @@
+import { Badge } from '@chakra-ui/react';
+
+interface Props {
+  score: number;
+}
+
+const CriticScore = ({ score }: Props) => {
+  const color = score > 90 ? 'green' : score > 80 ? 'yellow' : '';
+
+  return (
+    <Badge colorPalette={color} fontSize='14px' paddingX={2} borderRadius={4}>
+      {score}
+    </Badge>
+  );
+};
+
+export default CriticScore;
